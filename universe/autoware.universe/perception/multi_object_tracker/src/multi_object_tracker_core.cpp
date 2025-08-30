3f88f36e84c24d2bd75f4604d97a2268c2ae9981
Joonki0111 <hjk3889@chosun.kr>
2025-08-30 16:37:33 +0900

    fix: revert measurement_count_threshold for perception stability

--- universe/autoware.universe/perception/multi_object_tracker/src/multi_object_tracker_core.cpp
+++ universe/autoware.universe/perception/multi_object_tracker/src/multi_object_tracker_core.cpp
@@ -429,7 +429,7 @@ void MultiObjectTracker::sanitizeTracker(
 inline bool MultiObjectTracker::shouldTrackerPublish(
   const std::shared_ptr<const Tracker> tracker) const
 {
-  constexpr int measurement_count_threshold = 0;
+  constexpr int measurement_count_threshold = 3;
   if (tracker->getTotalMeasurementCount() < measurement_count_threshold) {
     return false;
   }
